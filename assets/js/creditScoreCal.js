0802b09747475a07cfde3a2a1de16e71c8092ea2
jiaruey97 <jiaruey.tan.2019@sis.smu.edu.sg>
2022-03-15 23:41:18 +0800

    calculation formula in

--- assets/js/creditScoreCal.js
+++ assets/js/creditScoreCal.js
@@ -1,23 +1,3 @@
-function creditScoreCal() {
-  var height = Number(document.getElementById("Height").value);
-  var weight = Number(document.getElementById("Weight").value);
-  var result = document.getElementById("bmi");
-  if (height == "" || weight == "") {
-    result.innerHTML = "current BMI: ";
-  } else {
-    var bmi = weight / height ** 2;
-    var bmi = bmi.toFixed(1);
-    if (bmi < 18.5) {
-      result.innerHTML = "current BMI: " + bmi + "(Underweight)";
-    } else if (bmi < 23) {
-      result.innerHTML = "current BMI: " + bmi + "(Healthy)";
-    } else if (bmi < 27.5) {
-      result.innerHTML = "current BMI: " + bmi + "(Mildly Obese)";
-    } else {
-      result.innerHTML = "current BMI: " + bmi + "(Extremely Obese)";
-    }
-  }
-}
 var annualIncome = document.getElementById("annualIncome").value;
 var homeOwnership = document.getElementById("homeOwnership").value;
 var monthlyDebt = document.getElementById("monthlyDebt").value;
@@ -34,13 +14,11 @@ function get_dti() {
   var current_dti = 0;
   if (monthly_inc == 0 && monthlyDebt != 0) {
     current_dti = 0;
-  } 
-  else if (monthlyDebt != 0) {
+  } else if (monthlyDebt != 0) {
     current_dti = monthlyDebt / monthly_inc;
   }
   return current_dti * 100;
 }
-
 var dti = get_dti(annualIncome, monthlyDebt);
 var current_dti = dti.toFixed(2);
 
@@ -49,13 +27,11 @@ function get_util_rate() {
   util_rate = 0;
   if (monthlyCreditLimit == 0 && monthlyDebt != 0) {
     var util_rate = 0;
+  } else if (monthlyDebt != 0) {
+    util_rate = monthlyDebt / monthlyCreditLimit;
   }
-  else if (monthlyDebt != 0) {
-    util_rate = (monthlyDebt / monthlyCreditLimit);
-}
   return util_rate * 100;
 }
-
 var util = get_util_rate(monthlyCreditLimit, monthlyDebt);
 var util_rate = util.toFixed(2);
 
@@ -73,7 +49,6 @@ function calc_annual_income_point() {
     return "Seems like you keyed an negative value";
   }
 }
-
 var annual_inc_point = calc_annual_income_point(annualIncome);
 
 function calc_dti_point() {
@@ -91,7 +66,6 @@ function calc_dti_point() {
     return "Seems like you keyed an negative value";
   }
 }
-
 var dti_point = calc_dti_point(dti);
 
 function calc_util_point() {
@@ -109,7 +83,6 @@ function calc_util_point() {
     return "Seems like you keyed an negative value";
   }
 }
-
 var util_point = calc_util_point(util);
 
 function calc_loan_amount_point() {
@@ -125,7 +98,6 @@ function calc_loan_amount_point() {
     return "Seems like you keyed an negative value";
   }
 }
-
 var loan_amount_point = calc_loan_amount_point(loanAmt);
 
 function calc_int_rate_point() {
@@ -143,5 +115,56 @@ function calc_int_rate_point() {
     return "Seems like you keyed an negative value";
   }
 }
-
 var int_rate_point = calc_int_rate_point(int_rate);
+
+function calc_mort_acc_point() {
+  if (0 <= mortAcc < 1.0) {
+    return 54;
+  } else if (1.0 <= mortAcc < 3.0) {
+    return 62;
+  } else if (3.0 <= mortAcc < 5.0) {
+    return 68;
+  } else if (mortAcc >= 5.0) {
+    return 72;
+  } else {
+    return "Seems like you keyed an negative value";
+  }
+}
+var mort_acc_point = calc_mort_acc_point(mortAcc);
+
+function calc_home_point() {
+  if (homeOwnership == "MORTGAGE") {
+    return 68;
+  } else if (homeOwnership == "OWN") {
+    return 59;
+  } else if (homeOwnership == "RENT") {
+    return 52;
+  } else {
+    return "Seems like you keyed an negative value";
+  }
+}
+var home_point = calc_home_point(homeOwnership);
+
+function calc_term_point() {
+  if (loanTerm == "36 months") {
+    return 68;
+  } else if (loanTerm == "60 months") {
+    return 59;
+  } else {
+    return "Seems like you keyed an negative value";
+  }
+}
+var term_point = calc_term_point(loanTerm);
+
+function creditScoreCal() {
+  var total =
+    annual_inc_point +
+    dti_point +
+    util_point +
+    int_rate_point +
+    mort_acc_point +
+    home_point +
+    term_point +
+    loan_amount_point;
+  return total;
+}
