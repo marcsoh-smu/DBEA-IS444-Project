7b9874ec01937e0d545df897111dff5b6deb1b81
marcsoh-smu <71428250+marcsoh-smu@users.noreply.github.com>
2022-03-18 23:08:20 +0800

    added CreditScore Href to the NavBar

--- assets/js/checkStatus.js
+++ assets/js/checkStatus.js
@@ -1,7 +1,7 @@
 function changeNavBar() {
-  // var optionalLogin = document.getElementsByClassName("optionalLogin")[0];
-  // var optionalLogout = document.getElementsByClassName("optionalLogout")[0];
-  // var optionalProfile = document.getElementsByClassName("optionalProfile")[0];
+  var optionalLogin = document.getElementsByClassName("optionalLogin")[0];
+  var optionalLogout = document.getElementsByClassName("optionalLogout")[0];
+  var optionalProfile = document.getElementsByClassName("optionalProfile")[0];
   // var optionalPostNewBlog = document.getElementsByClassName(
   //   "optionalPostNewBlog"
   // )[0];
@@ -15,15 +15,15 @@ function changeNavBar() {
       var result = this.responseText; //string
       if (result == "false") {
         //display new user page (hide log out and profile and new blog post)
-        // optionalLogin.id = "";
-        // optionalLogout.id = "optionalLogout";
-        // optionalProfile.id = "optionalProfile";
+        optionalLogin.id = "";
+        optionalLogout.id = "optionalLogout";
+        optionalProfile.id = "optionalProfile";
         // optionalMealPrep.id = "optionalMealPrep";
       } else {
         //display logged in page (hide login/sign )
-        // optionalLogin.id = "optionalLogin";
-        // optionalLogout.id = "";
-        // optionalProfile.id = "";
+        optionalLogin.id = "optionalLogin";
+        optionalLogout.id = "";
+        optionalProfile.id = "";
         // optionalMealPrep.id = "";
       }
     }
